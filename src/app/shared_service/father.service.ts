5f9693db526d4ad328f723d2976c33ea7962bc0a
Radosław Kwiatkowski <35296705+radkwiat84@users.noreply.github.com>
2018-09-05 15:15:29 +0200

    add father.service + bootsrap

--- src/app/shared_service/father.service.ts
+++ src/app/shared_service/father.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Father } from '../father';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FatherService {
+  private baseUrl: string = 'http://localhost:8080/father';
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+  private options = new RequestOptions({ headers: this.headers });
+  private father:Father;
+  constructor(private _http: Http) { }
 
-  constructor() { }
+  createFather(father: Father) {
+    return this._http.post(this.baseUrl + '/createFather', this.options)
+      .pipe(map((response: Response) => response.json()), catchError(this.errorHandler));
+  }
+
+  getFathers() {
+    return this._http.get(this.baseUrl + '/fathers', this.options)
+      .pipe(map((response: Response) => response.json()), catchError(this.errorHandler));
+
+  }
+
+  errorHandler(error: Response) {
+    return throwError(error || "SERVER ERROR");
+  }
+
+
+
+
+
 }
