ad3060802f622320d8b04bf60f59ed61b3ffbaf9
Radosław Kwiatkowski <35296705+radkwiat84@users.noreply.github.com>
2018-09-06 11:17:19 +0200

    create child.service, and added path in appRoutes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { IndexComponent } from './components/index/index.component';
 import { CreateFatherComponent } from './components/create-father/create-father.component';
@@ -14,6 +13,7 @@ import { CreateChildComponent } from './components/create-child/create-child.com
 const appRoutes:Routes=[
   {path:'', component:IndexComponent},
   {path:'createFather', component:CreateFatherComponent}
+  {path:'createChild', component:CreateChildComponent}
 ]
 
 
