546d6989efb813d29ce8dad05c8e03ff8a203e01
Radosław Kwiatkowski <35296705+radkwiat84@users.noreply.github.com>
2018-09-06 11:37:32 +0200

    add settings, and createChild(), setter(), getter() into child.service

--- src/app/shared_service/child.service.ts
+++ src/app/shared_service/child.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Http, RequestOptions, Headers, Response } from '@angular/http';
+import { Child } from '../child';
+import { map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChildService {
+  private baseUrl: string = 'http://localhost:8080/child';
+  private headers = new Headers({'Content-Type':'application/json'});
+  private options = new RequestOptions({headers: this.headers});
+  private child: Child;
 
-  constructor() { }
+  constructor(private _http: Http) { }
+
+  createChild(child: Child){
+    return this._http.post(this.baseUrl+'/createChild', JSON.stringify(child), this.options)
+      .pipe(map((response: Response)=> response.json()), catchError(this.errorHandler));
+  }
+
+  errorHandler(error: Response){
+    return throwError(error||"SERVER ERROR");
+  }
+
+  setter(child: Child){
+    this.child = child;
+  }
+
+  getter(){
+    return this.child;
+  }
+
 }
